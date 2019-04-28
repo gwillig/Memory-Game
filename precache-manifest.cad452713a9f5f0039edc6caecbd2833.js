18d0e88b7f7bd864d773c82ce7b124b50b03d666
gwillig <gustavwillig@gmail.com>
2019-04-28 13:35:09 +0200

    Updates

---
+++ precache-manifest.cad452713a9f5f0039edc6caecbd2833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e43d88f881cd76582ccc00b03ba5fe",
+    "url": "/gwillig/Memory-Game/index.html"
+  },
+  {
+    "revision": "ac6da878c2acf2d3b8c4",
+    "url": "/gwillig/Memory-Game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gwillig/Memory-Game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ac6da878c2acf2d3b8c4",
+    "url": "/gwillig/Memory-Game/static/js/main.a75b1002.chunk.js"
+  },
+  {
+    "revision": "b51bfe4ef7dd2e5ba112",
+    "url": "/gwillig/Memory-Game/static/js/runtime~main.3de81cbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gwillig/Memory-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
